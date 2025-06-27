64418dcc25b1f055163ae5851b60a6652eb7689e
Laura Anton <lauraamps@gmail.com>
2025-06-27 19:05:01 +0200

    modificaciones

--- staff/Laura-Anton/project/App/vite.config.js
+++ staff/Laura-Anton/project/App/vite.config.js
@@ -1,1 +1,7 @@
-import { }
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig ({
+    plugins: [ react(), tailwindcss()]
+})
