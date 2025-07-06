295c0a7c0e653e419580e4c979d6676335e76b54
Laura Anton <lauraamps@gmail.com>
2025-07-06 21:33:23 +0200

    modified middlewares, add BottomNav #98

---
+++ staff/Laura-Anton/project/App/view/components/BottomNav.jsx
@@ -0,0 +1,32 @@
+import { AiOutlineHome, AiOutlineSearch, AiOutlineShoppingCart, AiOutlineUser } from 'react-icons/ai'
+import { useNavigate } from 'react-router'
+
+
+export function BottomNav() {
+    const navigate = useNavigate()
+
+
+    return (
+        <nav className="fixed bottom-0 left-0 w-full bg-white border-t shadow-md py-2 flex justify-around items-center">
+            <button onClick={() => navigate('/Inicio')}>
+                <AiOutlineHome size={24} />
+            </button>
+
+            <button onClick={() => navigate('/buscar')}>
+                <AiOutlineSearch size={24} />
+            </button>
+
+            <button onClick={() => navigate('/menu')}>
+                <span className="text-xs font-semibold">MENU</span>
+            </button>
+
+            <button onClick={() => navigate('/carrito')}>
+                <AiOutlineShoppingCart size={24} />
+            </button>
+
+            <button onClick={() => navigate('/login')}>
+                <AiOutlineUser size={24} />
+            </button>
+        </nav>
+    )
+}
