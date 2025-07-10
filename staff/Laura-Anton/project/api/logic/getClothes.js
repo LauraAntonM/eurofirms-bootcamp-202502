d41e83e5980eb5a7e02439707e845aedad0b8874
Laura Anton <lauraamps@gmail.com>
2025-07-10 17:33:56 +0200

    modified logic in api #98

--- staff/Laura-Anton/project/api/logic/getClothes.js
+++ staff/Laura-Anton/project/api/logic/getClothes.js
@@ -1,25 +1,27 @@
 import { User, Clothe } from '../data/index.js'
-import { validate, SystemError, NotFoundError, AuthorshipError } from 'com'
+import { validate, SystemError, NotFoundError } from 'com'
 
-
-export const getClothes = (userId, category)  => {
+export const getClothes = (userId, category) => {
     validate.id(userId)
-    validate.category(category.toUpperCase())
 
     return User.findById(userId)
-        .catch(error => { throw new SystemError('mongo error') })
+        .catch(() => { throw new SystemError('mongo error') })
         .then(user => {
             if (!user) throw new NotFoundError('user not found')
 
-            return Clothe.find({category: category.toUpperCase()}).select('-__v').lean()
-                .catch(error => { throw new SystemError('mongo error') })
+            const query = {}
+
+            if (category) {
+                validate.category(category.toUpperCase())
+                query.category = category.toUpperCase()
+            }
+
+            return Clothe.find(query).select('-__v').lean()
+                .catch(() => { throw new SystemError('mongo error') })
                 .then(clothes => {
                     clothes.forEach(clothe => {
                         clothe.id = clothe._id.toString()
                         delete clothe._id
-
-                        
-
                     })
 
                     return clothes
